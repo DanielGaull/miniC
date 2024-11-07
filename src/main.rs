a9c38eb631737ee513cddea37bc52db435ff672a
Daniel Gaull <gaulldj@rose-hulman.edu>
2024-11-07 15:48:53 -0500

    Starting grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use pest::iterators::Pair;
+use std::fs;
+use pest::Parser;
+use pest_derive::Parser;
+#[derive(Parser)]
+#[grammar = "grammar.pest"] // relative to src
+struct MiniCParser;
+
 fn main() {
     println!("Hello, world!");
 }
