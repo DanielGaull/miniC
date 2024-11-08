1a63f1c4347794e599b2cc9f3884317c9cff66ef
Daniel Gaull <gaulldj@rose-hulman.edu>
2024-11-08 13:05:23 -0500

    Top level datagen

--- src/parsing/ast/toplevel.rs
+++ src/parsing/ast/toplevel.rs
@@ -1,3 +1,5 @@
+use crate::codegen::simple::SimpleCodeGen;
+
 use super::{expression::Expression, types::Type};
 
 pub enum TopLevel {
@@ -6,5 +8,41 @@ pub enum TopLevel {
         name: String,
         right: Option<Expression>,
     },
-    Import(String),
+    Import {
+        name: String,
+        is_lib: bool,
+    },
+}
+impl SimpleCodeGen for TopLevel {
+    fn generate(&self) -> String {
+        match self {
+            TopLevel::VarDeclaration { typ, name, right } => {
+                let mut s = String::new();
+                s.push_str(typ.generate().as_str());
+                s.push_str(" ");
+                s.push_str(name.as_str());
+                if let Some(value) = right {
+                    s.push_str(" = ");
+                    s.push_str(value.generate().as_str());
+                }
+                s.push_str(";");
+                s
+            },
+            TopLevel::Import { name, is_lib } => {
+                let mut s = String::new();
+                s.push_str("#import ");
+                if *is_lib {
+                    s.push_str("<");
+                    s.push_str(name.as_str());
+                    s.push_str(">");
+                } else {
+                    s.push_str("\"");
+                    s.push_str(name.as_str());
+                    s.push_str("\"");
+                }
+                s.push_str(";");
+                s
+            },
+        }
+    }
 }
