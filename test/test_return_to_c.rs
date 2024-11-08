d0cb569254231dbd0376dd886e99e978991d76a8
Daniel Gaull <gaulldj@rose-hulman.edu>
2024-11-07 21:52:58 -0500

    Added file for tests

---
+++ test/test_return_to_c.rs
@@ -0,0 +1,5 @@
+/**
+ * This is where we'll test C files that should simply output the same code
+ * i.e. we won't use any of the features of miniC, just simple programs
+ * that will test out a variety of types of expressions
+ */
